467049c28ed13f01ff408e8bad3781356d4d3131
veith <veithz@gmail.com>
2019-01-10 13:27:33 +0100

    tags proto added

--- proto/healthcheck/healthcheck.pb.go
+++ proto/healthcheck/healthcheck.pb.go
@@ -101,9 +101,9 @@ var _HealthCheck_serviceDesc = grpc.ServiceDesc{
 	Metadata: "healthcheck.proto",
 }
 
-func init() { proto.RegisterFile("healthcheck.proto", fileDescriptor_healthcheck_f73e1e794770af2d) }
+func init() { proto.RegisterFile("healthcheck.proto", fileDescriptor_healthcheck_37f6506448c5d979) }
 
-var fileDescriptor_healthcheck_f73e1e794770af2d = []byte{
+var fileDescriptor_healthcheck_37f6506448c5d979 = []byte{
 	// 171 bytes of a gzipped FileDescriptorProto
 	0x1f, 0x8b, 0x08, 0x00, 0x00, 0x00, 0x00, 0x00, 0x02, 0xff, 0xe2, 0x12, 0xcc, 0x48, 0x4d, 0xcc,
 	0x29, 0xc9, 0x48, 0xce, 0x48, 0x4d, 0xce, 0xd6, 0x2b, 0x28, 0xca, 0x2f, 0xc9, 0x17, 0xe2, 0x4f,
